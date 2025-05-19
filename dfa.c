cd174417a51c72e6cecb423b3bbd8181633bca88
ioseliev <ioseliev@pm.me>
2025-05-19 01:01:52 +0000

    Add Thompson constructions capabilities for DFA composition

--- dfa.c
+++ dfa.c
@@ -1,8 +1,13 @@
 #include "dfa.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <memory.h>
+#include <string.h>
 
+
+/* Constructor */
 
 DFA_t DFA(uint8_t n_states, uint16_t n_transitions) {
     bool *fs = calloc(n_states + 1, sizeof(bool)); // + 1 for stat
@@ -10,6 +15,8 @@ DFA_t DFA(uint8_t n_states, uint16_t n_transitions) {
     DFA_t ret = {n_states, fs, n_transitions, t};
     return ret;
 }
+
+/* Simulation */
 
 bool accepts(DFA_t *dfa, const char *input) {
     bool *finals = dfa->final_states;
@@ -21,6 +28,7 @@ bool accepts(DFA_t *dfa, const char *input) {
     while ((c = *(input++)) != '\0' && state != 0) {
         for (size_t i = 0; i < n_trans; ++i) {
             if (trans[i].from == state && trans[i].on == c) {
+                printf("%hhd -> %c -> %hhd\n", trans[i].from, c, trans[i].to);
                 state = trans[i].to;
                 break;
             }
@@ -32,4 +40,102 @@ bool accepts(DFA_t *dfa, const char *input) {
         }
     }
     return finals[state];
+}
+
+/* Thompson constructions */
+
+DFA_t singleton(char c) {
+    DFA_t ret = DFA(2, 1);
+    SET_FINAL(&ret, 2, true);
+    SET_TRANSITION(&ret, 0, 1, 2, c);
+    return ret;
+}
+
+DFA_t concat(DFA_t *s, DFA_t *t) {
+    DFA_t ret = DFA(s->n_states + t->n_states - 1, s->n_transitions + t->n_transitions);
+    uint8_t state_shift = 0;
+    uint16_t trans_shift = 0;
+
+    memcpy(ret.transitions + trans_shift, s->transitions, s->n_transitions * sizeof(Transition_t));
+
+    state_shift += s->n_states;
+    --state_shift;
+    trans_shift += s->n_transitions;
+    
+    memcpy(ret.transitions + trans_shift, t->transitions, t->n_transitions * sizeof(Transition_t));
+
+    for (uint16_t i = trans_shift; i < (trans_shift + t->n_transitions); ++i) {
+        ret.transitions[i].from += state_shift;
+        ret.transitions[i].to += state_shift;
+    }
+
+    state_shift += t->n_states;
+
+    SET_FINAL(&ret, state_shift, true);
+
+    return ret;
+}
+
+DFA_t join(DFA_t *s, DFA_t *t) {
+    DFA_t ret = DFA(s->n_states + t->n_states + 2, s->n_transitions + t->n_transitions + 4);
+    uint8_t state_shift = 1;
+    uint16_t trans_shift = 2;
+
+    SET_TRANSITION(&ret, 0, 1, 1 + state_shift, EPSILON);
+
+    memcpy(ret.transitions + trans_shift, s->transitions, s->n_transitions * sizeof(Transition_t));
+
+    for (uint16_t i = trans_shift; i < (trans_shift + s->n_transitions); ++i) {
+        ret.transitions[i].from += state_shift;
+        ret.transitions[i].to += state_shift;
+    }
+
+    state_shift += s->n_states;
+    trans_shift += s->n_transitions;
+
+    SET_TRANSITION(&ret, 1, 1, 1 + state_shift, EPSILON);
+
+    memcpy(ret.transitions + trans_shift, t->transitions, t->n_transitions * sizeof(Transition_t));
+
+    for (uint16_t i = trans_shift; i < (trans_shift + s->n_transitions); ++i) {
+        ret.transitions[i].from += state_shift;
+        ret.transitions[i].to += state_shift;
+    }
+
+    state_shift += t->n_states;
+    trans_shift += t->n_transitions;
+
+    SET_TRANSITION(&ret, trans_shift, 1 + state_shift - t->n_states - 1, 1 + state_shift, EPSILON);
+    SET_TRANSITION(&ret, trans_shift + 1, 1 + state_shift - 1, 1 + state_shift, EPSILON);
+
+    SET_FINAL(&ret, 1 + state_shift, true);
+
+    return ret;
+}
+
+DFA_t kleene(DFA_t *s) {
+    DFA_t ret = DFA(s->n_states + 2, s->n_transitions + 4);
+    uint8_t state_shift = 1;
+    uint16_t trans_shift = 2;
+
+    SET_TRANSITION(&ret, 0, 1, 1 + state_shift, EPSILON);
+
+    memcpy(ret.transitions + trans_shift, s->transitions, s->n_transitions * sizeof(Transition_t));
+
+    for (uint16_t i = trans_shift; i < (trans_shift + s->n_transitions); ++i) {
+        ret.transitions[i].from += state_shift;
+        ret.transitions[i].to += state_shift;
+    }
+
+    state_shift += s->n_states;
+    trans_shift += s->n_transitions;
+
+    SET_TRANSITION(&ret, trans_shift, 1 + state_shift - 1, 1 + state_shift - s->n_states, EPSILON);
+    SET_TRANSITION(&ret, trans_shift + 1, 1 + state_shift - 1, 1 + state_shift, EPSILON);
+
+    SET_TRANSITION(&ret, 1, 1, 1 + state_shift, EPSILON);
+
+    SET_FINAL(&ret, 1 + state_shift, true);
+
+    return ret;
 }
--- dfa.h
+++ dfa.h
@@ -24,8 +24,31 @@ typedef struct {
     Transition_t *transitions;
 } DFA_t;
 
+#define IS_FINAL(dfa, state) ((dfa)->final_states[(state)] == true)
+#define SET_FINAL(dfa, state, final) ((dfa)->final_states[(state)] = final)
+#define SET_TRANSITION(dfa, index, from_, to_, on_)   \
+    {                                              \
+        (dfa)->transitions[(index)].from = (from_); \
+        (dfa)->transitions[(index)].to = (to_);     \
+        (dfa)->transitions[(index)].on = (on_);     \
+    }
+
+/* Constructor */
+
 DFA_t DFA(uint8_t n_states, uint16_t n_transitions);
 
+/* Simulation */
+
 bool accepts(DFA_t *dfa, const char *input);
+
+/* Thompson constructions */
+
+DFA_t singleton(char c);
+
+DFA_t concat(DFA_t *s, DFA_t *t);
+
+DFA_t join(DFA_t *s, DFA_t *t);
+
+DFA_t kleene(DFA_t *s);
 
 #endif
