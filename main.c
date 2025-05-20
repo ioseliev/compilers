48552c64c94537446a3c8f4181115d88727cf356
ioseliev <ioseliev@pm.me>
2025-05-20 02:40:39 +0000

    Make tokens' expressions available to main

--- main.c
+++ main.c
@@ -1,10 +1,41 @@
 #include "utils.h"
 #include "regex.h"
 #include "dfa.h"
+#include "token.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
+
+static const struct { TokenType token; const char *regex; } EXPRESSIONS[] = {
+    /* Keywords */
+    { KW_NUM,  "num" },
+    { KW_TEXT, "text" },
+    { KW_SHOW, "show" },
+ 
+    /* Literals */
+    { NUM_LITERAL, "[0-9][0-9]*" },
+    { TEXT_LITERAL, "\"[^\"]*\"" },
+
+    /* Identifiers */
+    /* NOTE: It's not specified whether identifiers can contain reserved sym- 
+     * bols like numbers or operators and whether can begin with them. As a
+     * result, a conservative interpretation was taken.
+     */
+    { ID, "[a-zA-Z][a-zA-Z!@#$%&?/_|]{0,29}" },
+
+    /* Operators */
+    { OP_ADD, "+" },
+    { OP_SUB, "-" },
+    { OP_MUL, "\\*" },
+    { OP_DIV, "/" },
+    { OP_LT, "<" },
+    { OP_GT, ">" },
+    { OP_ASSIGN, "=" },
+
+    /* Punctuation */
+    { SEMICOLON, ";" }
+};
 
 int main(int argc, char **argv) {
     /* const char *source = NULL;
